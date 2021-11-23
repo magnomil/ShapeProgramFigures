02b5f0804530a3699b0f1989ca6f8730bd028817
magnomil <67349729+magnomil@users.noreply.github.com>
2021-11-22 23:20:06 -0300

    Adicionar arquivos de projeto.

---
+++ ShapeProgramFigures/Entities/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using ShapeProgramFigures.Entities.Enuns;
+
+namespace ShapeProgramFigures.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+        
+        public Circle (double radius, Color color): base(color)
+        {
+            Radius = radius;
+        }
+
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
---
+++ ShapeProgramFigures/Entities/Shape.cs
@@ -0,0 +1,28 @@
+﻿using ShapeProgramFigures.Entities.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeProgramFigures.Entities
+{
+    abstract class Shape
+    {
+        public Color color { get; set; }
+        public Color Color { get; }
+
+        public Shape (Color color)
+        {
+            Color = color;
+        }
+
+
+
+
+        public abstract double Area();
+    }
+    
+}
+
+    
